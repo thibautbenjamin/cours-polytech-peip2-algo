67241b5989ebac899ddf1f174fa176c3a86b4b29
HiAbdounour <abdounour.misbah@universite-paris-saclay.fr>
2025-11-26 21:59:52 +0100

    mauvais for est possible

---
+++ code/mauvais_for.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // Ceci est une mauvaise boucle for
+    for(int i=0;i<=10;i++){
+        cout << "Bonjour !" << endl;
+        i=0;
+    }
+    return 0;
+}
