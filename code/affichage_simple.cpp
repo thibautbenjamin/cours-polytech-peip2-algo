ae728e454b52377ec32692e2a137a5adba9dc135
HiAbdounour <abdounour.misbah@universite-paris-saclay.fr>
2025-11-26 22:06:49 +0100

    terminaison boucles for

---
+++ code/affichage_simple.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // Affiche tous les nombres de 0 à 10
+    for(int i=0;i<=10;i++){
+        cout << i << endl;
+    }
+    return 0;
+}
